96dd20baf7ce0f5f36d3b2c81c374e7b249850e1
giselerezende2 <gisele.carvalho@sptech.school>
2025-03-13 15:22:22 -0300

    Criando Estrutura do Projeto com Spring Boot
    Criando o Projeto com Spring Boot 3.4.3, utilizando JDK 21 e o Maven como Gerenciador de Dependências do Projeto.

---
+++ beiramar.api/src/main/java/com/beiramar/beiramar/api/BeiraMarApplication.java
@@ -0,0 +1,13 @@
+package com.beiramar.beiramar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeiraMarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeiraMarApplication.class, args);
+	}
+
+}
